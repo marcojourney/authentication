d2e6ca89c98998eccb276cfc83c138c2e14b6263
Marco <mornsophannamis@gmail.com>
2023-08-21 17:07:13 +0700

    some modification

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,17 @@
+import { ConfigService } from "@nestjs/config";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(configService: ConfigService) {
+      super({
+        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+        ignoreExpiration: false,
+        secretOrKey: 'yoursecret'
+      });
+    }
+  
+    async validate(payload) {
+      return { id: payload.sub, user: payload.user};
+    }
+  }
